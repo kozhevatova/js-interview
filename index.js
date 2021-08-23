2355fb97a6e0e179fbd01961d449d6538a4c1576
kozhevatova <ad.kozhevatova@yandex.ru>
2021-08-23 16:54:55 +0300

    fix: функция проходит тесты

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const _ = require('lodash')
 
 function fetchGracePeriodLogicFromMapping (cardId, mapping = '') {
-    return "base"
+    let res = null;
+    const types = {};
+
+    mapping.split(';').reverse().forEach((str) => {
+        const tmp = str.split(':');
+        if(tmp[0]) {
+            //если элемент определен, записываем в объект пару, где ключ - логическое имя,
+            //а значение - массив id для этого имени из входных данных
+            types[tmp[0].trim()] = tmp[1].split(',');
+        };
+        
+    });
+    
+    for(let type in types) {
+        types[type].forEach((elem) => {
+            if(elem.trim() === cardId && !res) {
+                res = type;
+            }
+        });
+    }
+    
+    return res;
 }
 
 module.exports = fetchGracePeriodLogicFromMapping
